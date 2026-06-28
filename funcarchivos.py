83227ff0c8e6163a90d8eff5144b9cd44e914d55
Benjamin Durand <b.xjdurand@gmail.com>
2026-06-27 22:59:39 -0400

    Levantar error cuando no se encuentre archivo
    Arreglo para evitar que el programa deje de funcionar al no encontrar archivo disponible

--- funcarchivos.py
+++ funcarchivos.py
@@ -5,5 +5,8 @@ def guardar_datos(lista):
     with open("archivo.json", "w", encoding="utf-8") as i:
         json.dump(lista, i, indent=4)
 def cargar_datos():
-    with open("archivo.json", "r", encoding="utf-8") as i:
-        return json.load(i)
+    try:
+     with open("archivo.json", "r", encoding="utf-8") as i:
+         return json.load(i)
+    except FileNotFoundError:
+       print("No se encontró un archivo disponible")
